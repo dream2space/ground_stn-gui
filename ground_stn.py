c0b906a7dd73898d38c94fc4b8c953843f3a20f8
huiminlim <e0036379@u.nus.edu>
2021-05-06 18:31:39 +0800

    refactor changing value

--- ground_stn.py
+++ ground_stn.py
@@ -11,12 +11,21 @@ class MainApplication(tk.Frame):
         # Create a section/labelframe for beacon data
         self.beacon_frame = BeaconFrame(self.parent, text="Beacon Data")
 
+    def update_beacon_values(self, temp, gx, gy, gz):
+        self.beacon_frame.update_beacon_values(temp, gx, gy, gz)
+
 
 class BeaconFrame(tk.LabelFrame):
     def __init__(self, parent, *args, **kwargs):
         tk.LabelFrame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.pack(padx=10, pady=10)
+
+        # Create variable to store beacon values
+        self.temp = tk.StringVar()
+        self.gx = tk.StringVar()
+        self.gy = tk.StringVar()
+        self.gz = tk.StringVar()
 
         # Create label for beacon data header
         self.temperature_label = self._create_header_label("Temp")
@@ -25,19 +34,27 @@ class BeaconFrame(tk.LabelFrame):
         self.gz_label = self._create_header_label("GZ")
 
         # Create label to store beacon data
-        self.temp_text = self._create_text_label("37.60")
-        self.gx_text = self._create_text_label("-5")
-        self.gy_text = self._create_text_label("8")
-        self.gz_text = self._create_text_label("3")
+        self.temp_text = self._create_text_label(self.temp)
+        self.gx_text = self._create_text_label(self.gx)
+        self.gy_text = self._create_text_label(self.gy)
+        self.gz_text = self._create_text_label(self.gz)
 
         # Put the labels in grids with row/col
         self._arrange_grid()
 
+        self.update_beacon_values("0.0", "0", "0", "0")
+
+    def update_beacon_values(self, temp, gx, gy, gz):
+        self.temp.set(temp)
+        self.gx.set(gx)
+        self.gy.set(gy)
+        self.gz.set(gz)
+
     def _create_header_label(self, header_text):
         return tk.Label(self, width=6, text=header_text, borderwidth=1, relief="groove")
 
-    def _create_text_label(self, value_text):
-        return tk.Label(self, width=6, text=value_text,
+    def _create_text_label(self, text_container):
+        return tk.Label(self, width=6, textvariable=text_container,
                         bg="white", borderwidth=1, relief="groove")
 
     def _arrange_grid(self):
