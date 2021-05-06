e540e47a9e5e68ad7598e8c10a63765b425296ae
huiminlim <e0036379@u.nus.edu>
2021-05-06 22:47:51 +0800

    fix inheritance

--- GroundStationGUI.py
+++ GroundStationGUI.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 
 
-class GroundStationPage():  # (tk.Frame):
+class GroundStationPage(tk.Frame):
     def __init__(self, parent, pipe):
-        # tk.Frame.__init__(self, parent, *args, **kwargs)
+        tk.Frame.__init__(self, parent)
         self.parent = parent
         self.parent.minsize(450, 80)
         self.parent.title("Ground Station")
