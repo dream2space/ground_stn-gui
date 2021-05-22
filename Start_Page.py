e738b3157a12b3fb7bf3e582d915c18623522961
huiminlim <e0036379@u.nus.edu>
2021-05-22 17:46:48 +0800

    refactor

--- Start_Page.py
+++ Start_Page.py
@@ -23,11 +23,6 @@ class StartPage(tk.Frame):
         self.middle_continer.pack(padx=10)
         self.bottom_container.pack(side=tk.BOTTOM, padx=10, pady=5)
 
-        self.button_container = tk.Frame(self.middle_continer)
-        self.warning_container = tk.Frame(self.middle_continer)
-        self.warning_container.pack(side=tk.TOP)
-        self.button_container.pack(side=tk.BOTTOM)
-
         # Create welcome and logo
         welcome_fontStyle = tkFont.Font(
             family="Helvetica", size=14, weight="bold")
@@ -40,40 +35,44 @@ class StartPage(tk.Frame):
         self.image_logo.pack(side=tk.BOTTOM, fill="both",
                              expand="yes", padx=5, pady=5)
 
-        # Create left and right for bottom container
-        self.bottom_left = tk.Frame(self.middle_continer)
-        self.bottom_right = tk.Frame(self.middle_continer)
-        self.bottom_left.pack(side=tk.LEFT, padx=40)
-        self.bottom_right.pack(side=tk.RIGHT, padx=40)
-
         # Create a label for messages and warning
-        self.display_instruction_message()
+        self.warning_container = tk.Frame(self.middle_continer)
+        self.warning_container.pack(side=tk.TOP)
+        self.display_instruction_message(self.warning_container)
+
+        # Create left and right for bottom container
+        self.middle_left_container = tk.Frame(self.middle_continer)
+        self.middle_right_container = tk.Frame(self.middle_continer)
+        self.middle_left_container.pack(side=tk.LEFT, padx=40)
+        self.middle_right_container.pack(side=tk.RIGHT, padx=40)
 
         # Create label to prompt ttnc port selection
         self.ttnc_label = tk.Label(
-            self.bottom_left, text="Select TT&C COM port")
+            self.middle_left_container, text="Select TT&C COM port")
         self.ttnc_label.pack(side=tk.TOP)
 
         # Setup option menu for ttnc
         self.ttnc_value_in_menu = tk.StringVar()
         self.ttnc_value_in_menu.set(ports[0])
         self.ttnc_option_menu = tk.OptionMenu(
-            self.bottom_left, self.ttnc_value_in_menu, *ports)
+            self.middle_left_container, self.ttnc_value_in_menu, *ports)
         self.ttnc_option_menu.pack(side=tk.BOTTOM)
 
         # Create label to prompt payload port selection
         self.payload_label = tk.Label(
-            self.bottom_right, text="Select Payload COM port")
+            self.middle_right_container, text="Select Payload COM port")
         self.payload_label.pack(side=tk.TOP, anchor=tk.CENTER)
 
         # Setup option menu for payload
         self.payload_value_in_menu = tk.StringVar()
         self.payload_value_in_menu.set(ports[0])
         self.payload_option_menu = tk.OptionMenu(
-            self.bottom_right, self.payload_value_in_menu, *ports)
+            self.middle_right_container, self.payload_value_in_menu, *ports)
         self.payload_option_menu.pack(side=tk.BOTTOM)
 
         # Button to confirm choice
+        self.button_container = tk.Frame(self.middle_continer)
+        self.button_container.pack(side=tk.BOTTOM)
         self.button = tk.Button(self.button_container, text="Start",
                                 compound=tk.CENTER, command=controller.handle_transition)
         self.button.pack()
@@ -90,14 +89,14 @@ class StartPage(tk.Frame):
         self.warning_label.pack()
         self.after(2000, self.reset_instruction_message)
 
-    def display_instruction_message(self):
+    def display_instruction_message(self, parent):
         self.warning_fontStyle = tkFont.Font(
             family="TkDefaultFont", weight="bold", size=10)
         self.warning_text = tk.StringVar()
         self.warning_text.set(
             "To begin, select the COM ports for TT&C and Payload transceivers.")
         self.warning_label = tk.Label(
-            self.warning_container, textvariable=self.warning_text, compound=tk.CENTER, font=self.warning_fontStyle)
+            parent, textvariable=self.warning_text, compound=tk.CENTER, font=self.warning_fontStyle)
         self.warning_label.pack(side=tk.TOP, padx=3, pady=3)
 
     def reset_instruction_message(self):
