b9efc54127acd1e16641c4261c895bbad9cc8324
huiminlim <e0036379@u.nus.edu>
2021-05-23 19:07:19 +0800

    move examples into archive

--- examples/multi-page/testdriver.py
+++
@@ -1,9 +0,0 @@
-from TestApp import MainView
-import tkinter as tk
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    main = MainView(root)
-    main.pack(side="top", fill="both", expand=True)
-    root.wm_geometry("400x400")
-    root.mainloop()
