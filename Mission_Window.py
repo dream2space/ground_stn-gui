336207853a7c26b1e14fe794f42b571b94b95286
huiminlim <e0036379@u.nus.edu>
2021-05-25 14:46:19 +0800

    mission input parsing

--- Mission_Window.py
+++ Mission_Window.py
@@ -1,3 +1,4 @@
+import datetime
 import tkinter as tk
 from tkinter import ttk
 
@@ -165,8 +166,8 @@ class Mission:
         self._parse(mission_date, downlink_date, mission_time, downlink_time, image_count, interval)
 
     def __str__(self):
-        _1 = f"mission date: {'{:02d}'.format(self.mission_date_yyyy)}/{'{:02d}'.format(self.mission_date_MM)}/{'{:02d}'.format(self.mission_date_dd)} | mission time: {'{:02d}'.format(self.mission_time_hh)}:{'{:02d}'.format(self.mission_time_mm)}:{'{:02d}'.format(self.mission_time_ss)}\n"
-        _2 = f"downlink date: {'{:02d}'.format(self.downlink_date_yyyy)}/{'{:02d}'.format(self.downlink_date_MM)}/{'{:02d}'.format(self.downlink_date_dd)} | mission time: {'{:02d}'.format(self.downlink_time_hh)}:{'{:02d}'.format(self.downlink_time_mm)}:{'{:02d}'.format(self.downlink_time_ss)}\n"
+        _1 = f"mission date: {self.mission_datetime.strftime('%d/%m/%Y')} | mission time: {self.mission_datetime.strftime('%H:%M:%S')}\n"
+        _2 = f"downlink date: {self.downlink_datetime.strftime('%d/%m/%Y')} | mission time: {self.downlink_datetime.strftime('%H:%M:%S')}\n"
         _3 = f"image count: {self.image_count} | image interval: {self.interval}"
         return _1 + _2 + _3
 
@@ -183,24 +184,28 @@ class Mission:
             return time_split
 
         parsed_mission_date = _parse_date(mission_date)
-        self.mission_date_yyyy = int(parsed_mission_date[0])
-        self.mission_date_MM = int(parsed_mission_date[1])
-        self.mission_date_dd = int(parsed_mission_date[2])
+        parsed_mission_time = _parse_time(mission_time)
+        mission_date_yyyy = int(parsed_mission_date[0])
+        mission_date_MM = int(parsed_mission_date[1])
+        mission_date_dd = int(parsed_mission_date[2])
+        mission_time_hh = int(parsed_mission_time[0])
+        mission_time_mm = int(parsed_mission_time[1])
+        mission_time_ss = int(parsed_mission_time[2])
+        self.mission_datetime = datetime.datetime(
+            year=mission_date_yyyy, month=mission_date_MM, day=mission_date_dd, hour=mission_time_hh,
+            minute=mission_time_mm, second=mission_time_ss)
 
         parsed_downlink_date = _parse_date(downlink_date)
-        self.downlink_date_yyyy = int(parsed_downlink_date[0])
-        self.downlink_date_MM = int(parsed_downlink_date[1])
-        self.downlink_date_dd = int(parsed_downlink_date[2])
-
-        parsed_mission_time = _parse_time(mission_time)
-        self.mission_time_hh = int(parsed_mission_time[0])
-        self.mission_time_mm = int(parsed_mission_time[1])
-        self.mission_time_ss = int(parsed_mission_time[2])
-
         parsed_downlink_time = _parse_time(downlink_time)
-        self.downlink_time_hh = int(parsed_downlink_time[0])
-        self.downlink_time_mm = int(parsed_downlink_time[1])
-        self.downlink_time_ss = int(parsed_downlink_time[2])
+        downlink_date_yyyy = int(parsed_downlink_date[0])
+        downlink_date_MM = int(parsed_downlink_date[1])
+        downlink_date_dd = int(parsed_downlink_date[2])
+        downlink_time_hh = int(parsed_downlink_time[0])
+        downlink_time_mm = int(parsed_downlink_time[1])
+        downlink_time_ss = int(parsed_downlink_time[2])
+        self.downlink_datetime = datetime.datetime(
+            year=downlink_date_yyyy, month=downlink_date_MM, day=downlink_date_dd, hour=downlink_time_hh,
+            minute=downlink_time_mm, second=downlink_time_ss)
 
         self.image_count = int(image_count)
         self.interval = int(interval)
