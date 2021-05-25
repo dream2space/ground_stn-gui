10223e438ddbfe893a5040fa3566a1742047c969
huiminlim <e0036379@u.nus.edu>
2021-05-25 16:51:32 +0800

    fix button

--- Command_Panel.py
+++ Command_Panel.py
@@ -58,20 +58,25 @@ class MissionDownlinkFrame(tk.LabelFrame):
         tk.LabelFrame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.controller = controller
-        self.pack(side=tk.BOTTOM, anchor=tk.SW, expand=1, fill="both")
+        self.pack(side=tk.BOTTOM, anchor=tk.SW, fill="both")
 
         # Display table for pending mission
         self.pending_mission_table = MissionTable(self)
 
         # Add line for Mission and Downlink command
         self.label = tk.Label(
-            self, text="Send Mission + Downlink Command")
+            self, text="Send Mission + Downlink Command", pady=8)
         self.label.pack()
 
         # Add button to start process to send Mission and Downlink command
-        self.button = tk.Button(self, text="Click here", pady=8,
+        self.button = tk.Button(self, text="Click here",
                                 command=self.controller.open_mission_downlink_command_window)
         self.button.pack()
+
+        # Add a success message
+        self.success_message = tk.StringVar()
+        self.success_label = tk.Label(self, textvariable=self.success_message)
+        self.success_label.pack()
 
 
 class MissionTable(ttk.Treeview):
@@ -82,7 +87,7 @@ class MissionTable(ttk.Treeview):
         # Display currently pending missions
         col = (1, 2, 3)
         self.mission_pending_view = ttk.Treeview(self.parent, columns=col, show='headings', height=3)
-        self.mission_pending_view.pack()
+        self.mission_pending_view.pack(padx=2, pady=2)
 
         # Setup columns in treeview table
         for i in range(len(col)):
