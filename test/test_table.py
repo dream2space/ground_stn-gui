480f260f7bebb5ed5544f4b60e3fdfc5522a3bb3
huiminlim <e0036379@u.nus.edu>
2021-06-02 22:52:35 +0800

    add test table creation code

--- test/test_table.py
+++ test/test_table.py
@@ -1,5 +1,13 @@
+import os
+
 from tabulate import tabulate
 
 table = [["spam", 42], ["eggs", 451], ["bacon", 0]]
 headers = ["item", "qty"]
 print(tabulate(table, headers=headers, tablefmt="pretty"))
+print(type(tabulate(table, headers=headers, tablefmt="pretty")))
+
+# If no mission created yet, create file (should not exist yet)
+if not os.path.exists(f"status.txt"):
+    with open(f"status.txt", "w") as status_file:
+        status_file.write(tabulate(table, headers=headers, tablefmt="pretty"))
