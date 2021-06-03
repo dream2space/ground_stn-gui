2e91f0df9074404269733e8784446c0061dad67e
huiminlim <e0036379@u.nus.edu>
2021-06-03 17:36:00 +0800

    prettify mission status table

--- Mission_Status_Record.py
+++ Mission_Status_Record.py
@@ -15,29 +15,43 @@ class Mission_Status_Recorder():
         self.table_header = ['#', 'Downlink Status', 'Reed Solomon Decode Status', 'Unzip + Base64 Decode Status']
 
     def create_new_record(self, image_count):
-        self.image_status_record.append({'#': image_count, 'Downlink Status': False,
-                                         'Reed Solomon Decode Status': False, 'Unzip + Base64 Decode Status': False})
+        self.image_status_record.append(
+            {'#': image_count, 'Downlink Status': "FAILED", 'Reed Solomon Decode Status': "FAILED",
+             'Unzip + Base64 Decode Status': "FAILED"})
 
     def update_downlink_status(self, image_count, is_success):
         idx = image_count - 1
-        self.image_status_record[idx].update({'Downlink Status': is_success})
+        if is_success:
+            self.image_status_record[idx].update({'Downlink Status': "SUCCESS"})
+        else:
+            self.image_status_record[idx].update({'Downlink Status': "FAILED"})
 
     def update_rs_decode_status(self, image_count, is_success):
         idx = image_count - 1
-        self.image_status_record[idx].update({'Reed Solomon Decode Status': is_success})
+        if is_success:
+            self.image_status_record[idx].update({'Reed Solomon Decode Status': "SUCCESS"})
+        else:
+            self.image_status_record[idx].update({'Reed Solomon Decode Status': "FAILED"})
 
     def update_unzip_base64_decode_status(self, image_count, is_success):
         idx = image_count - 1
-        self.image_status_record[idx].update({'Unzip + Base64 Decode Status': is_success})
+        if is_success:
+            self.image_status_record[idx].update({'Unzip + Base64 Decode Status': "SUCCESS"})
+        else:
+            self.image_status_record[idx].update({'Unzip + Base64 Decode Status': "FAILED"})
 
     def create_mission_status_log(self):
         table_values = [list(x.values()) for x in self.image_status_record]
 
         # Create the log file
         with open(f"{app_params.GROUND_STN_MISSION_FOLDER_PATH}/{self.mission_name}/{self.mission_name}_status.txt", 'w') as status_log:
-            to_print = f"Mission Name: {self.mission_name}\n"
-            to_print += f"Mission Start time: {self.mission_start_time}\n"
-            to_print += f"Mission Downlink time: {self.mission_downlink_time}\n"
+            to_print = ""
+
+            status = [["Mission Name", self.mission_name],
+                      ["Mission Start time", self.mission_start_time],
+                      ["Mission Downlink time", self.mission_downlink_time]]
+            to_print += tabulate(status, tablefmt="pretty")
+
             to_print += '\n'
             to_print += 'Mission Status:\n'
             to_print += tabulate(table_values, headers=self.table_header, tablefmt="pretty")
