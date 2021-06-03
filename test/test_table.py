0b8eca9b0125a3039e5360037da573c5314e7415
huiminlim <e0036379@u.nus.edu>
2021-06-03 16:30:36 +0800

    add test mission code print

--- test/test_table.py
+++ test/test_table.py
@@ -1,13 +1,23 @@
-import os
+# import os
 
 from tabulate import tabulate
 
-table = [["spam", 42], ["eggs", 451], ["bacon", 0]]
-headers = ["item", "qty"]
-print(tabulate(table, headers=headers, tablefmt="pretty"))
-print(type(tabulate(table, headers=headers, tablefmt="pretty")))
+table = []
+table.append({'#': 0, 'Downlink Status': False,
+              'Reed Solomon Decode Status': False, 'Unzip + Base64 Decode Status': False})
+table.append({'#': 0, 'Downlink Status': False,
+              'Reed Solomon Decode Status': False, 'Unzip + Base64 Decode Status': False})
+table.append({'#': 0, 'Downlink Status': False,
+              'Reed Solomon Decode Status': False, 'Unzip + Base64 Decode Status': False})
+table.append({'#': 0, 'Downlink Status': False,
+              'Reed Solomon Decode Status': False, 'Unzip + Base64 Decode Status': False})
+
+table_values = [list(x.values()) for x in table]
+headers = ['#', 'Downlink Status', 'Reed Solomon Decode Status', 'Unzip + Base64 Decode Status']
+print(tabulate(table_values, headers=headers, tablefmt="pretty"))
+# print(type(tabulate(table, headers=headers, tablefmt="pretty")))
 
 # If no mission created yet, create file (should not exist yet)
-if not os.path.exists(f"status.txt"):
-    with open(f"status.txt", "w") as status_file:
-        status_file.write(tabulate(table, headers=headers, tablefmt="pretty"))
+# if not os.path.exists(f"status.txt"):
+#     with open(f"status.txt", "w") as status_file:
+#         status_file.write(tabulate(table, headers=headers, tablefmt="pretty"))
